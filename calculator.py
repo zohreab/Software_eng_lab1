f2e8566b9f25877a625c72358a1398ab0c7f5206
zohre <zohreabbasiliasi@gmail.com>
2023-07-14 21:21:32 +0900

    modifying environment

--- calculator.py
+++ calculator.py
@@ -12,21 +12,22 @@ def calculator():
 
     choice = int(input("Please enter your choice number (1-6): "))
 
-    num1 = input("Please enter the first number (for choices from 4 to 6 the first number is the base): ")
-    num2 = input("Please enter the second number: ")
+    num1 = float(input("Please enter the first number (for choices from 4 to 6 the first number is the base): "))
+    num2 = float(input("Please enter the second number: "))
 
     if choice == 1:
-        Add(num1, num2)
+        result = add(num1, num2)
+        print(f"Result: {result}")
     elif choice == 2:
-        Sub(num1, num2)
+        sub(num1, num2)
     elif choice == 3:
-        Mul(num1, num2)
+        mul(num1, num2)
     elif choice == 4:
-        Div(num1, num2)
+        div(num1, num2)
     elif choice == 5:
-        Power(num1, num2)
+        power(num1, num2)
     elif choice == 6:
-        Root(num1, num2)
+        root(num1, num2)
     else:
         print("your Input is invalid!")
 
